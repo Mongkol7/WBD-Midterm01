e7a8e6c02865bff8ba59ca95a751ae97ac93ee5d
Mongkol7 <thoeungsereymongkol@gmail.com>
2025-10-25 15:13:45 +0700

    second

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const http = require('http');
-const url = require('url');
+const express = require('express');
 const { handleRoute } = require('./routes');
 
-const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-
-  handleRoute(parsedUrl.pathname, res);
-});
-
+const app = express();
 const port = process.env.PORT || 4000;
 
-server.listen(port, () => {
-  console.log(`TechShop server running at http://localhost:${port}`);
+app.use(express.json());
+
+// Handle routes
+app.get('*', (req, res) => {
+  handleRoute(req.path, res);
 });
 
-server.on('error', (error) => {
-  if (error.code === 'EADDRINUSE') {
-    console.log(`Port ${port} is busy, trying ${port + 1}...`);
-    server.listen(port + 1);
-  } else {
-    console.error('Server error:', error);
-  }
-});
-
+app
+  .listen(port, () => {
+    console.log(`TechShop server running at http://localhost:${port}`);
+  })
+  .on('error', (error) => {
+    if (error.code === 'EADDRINUSE') {
+      console.log(`Port ${port} is busy, trying ${port + 1}...`);
+      app.listen(port + 1);
+    } else {
+      console.error('Server error:', error);
+    }
+  });
